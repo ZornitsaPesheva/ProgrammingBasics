786535684a3b7f6ecd38ff278f9dba76b7ce4eac
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-09 15:48:38 +0300

    Plochki

---
+++ Exams/Sample Coding 101 Exam - 24 April 2016/plochki/plochki/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var n = double.Parse(Console.ReadLine());
+             var w = double.Parse(Console.ReadLine());
+             var l = double.Parse(Console.ReadLine());
+             var m = double.Parse(Console.ReadLine());
+             var o = double.Parse(Console.ReadLine());
+
+            var surface = n * n - m * o;
+            var plate = w * l;
+            var plates = surface / plate;
+            Console.WriteLine(plates);
+            var time = plates * 0.2;
+            Console.WriteLine(time);
+            Console.ReadLine();
+        }
+    }
+}
