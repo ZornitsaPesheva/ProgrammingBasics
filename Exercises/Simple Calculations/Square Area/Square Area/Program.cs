103ca7f8c171d4e81302bc1f401c725ec40bc0c2
ZornitsaSerbezova <pesheva@gmail.com>
2016-09-09 15:58:00 +0300

    Square area

---
+++ Exercises/Simple Calculations/Square Area/Square Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = double.Parse(Console.ReadLine());
+            var h = double.Parse(Console.ReadLine());
+            Console.WriteLine("Triangle area = " + Math.Round((a * h / 2), 2));
+            Console.ReadLine();
+        }
+    }
+}
